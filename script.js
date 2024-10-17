aba6f6f0baf29aa117be74e779afabd36b488da5
Sabbir128 <78898458+Sabbir128@users.noreply.github.com>
2024-10-18 02:30:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-// Initialize CodeMirror editors for HTML, CSS, and JavaScript
-let htmlEditor = CodeMirror.fromTextArea(document.getElementById("html-editor"), {
-    mode: "htmlmixed",
-    lineNumbers: true,
-    theme: "default"
+document.addEventListener("DOMContentLoaded", function() {
+    var htmlEditor = CodeMirror.fromTextArea(document.getElementById("htmlEditor"), {
+        mode: "htmlmixed",
+        lineNumbers: true,
+        theme: "default",
+    });
+    
+    var cssEditor = CodeMirror.fromTextArea(document.getElementById("cssEditor"), {
+        mode: "css",
+        lineNumbers: true,
+        theme: "default",
+    });
+    
+    var jsEditor = CodeMirror.fromTextArea(document.getElementById("jsEditor"), {
+        mode: "javascript",
+        lineNumbers: true,
+        theme: "default",
+    });
+    
+    document.getElementById("runBtn").addEventListener("click", function() {
+        var html = htmlEditor.getValue();
+        var css = "<style>" + cssEditor.getValue() + "</style>";
+        var js = "<script>" + jsEditor.getValue() + "<\/script>";
+        var output = document.getElementById("output");
+        output.contentDocument.body.innerHTML = html + css + js;
+    });
+
+    document.getElementById("newFileBtn").addEventListener("click", function() {
+        var newFileName = prompt("Enter new file name:");
+        if (newFileName) {
+            var fileList = document.getElementById("fileList");
+            var newFile = document.createElement("li");
+            newFile.textContent = newFileName;
+            newFile.onclick = function() {
+                alert("Open file: " + newFileName);
+            };
+            fileList.appendChild(newFile);
+            document.getElementById("fileManager").style.display = "block";
+        }
+    });
+
+    document.getElementById("saveFileBtn").addEventListener("click", function() {
+        alert("Saving file... (this is a stub for now)");
+    });
 });
 
-let cssEditor = CodeMirror.fromTextArea(document.getElementById("css-editor"), {
-    mode: "css",
-    lineNumbers: true,
-    theme: "default"
-});
-
-let jsEditor = CodeMirror.fromTextArea(document.getElementById("js-editor"), {
-    mode: "javascript",
-    lineNumbers: true,
-    theme: "default"
-});
-
-// Function to switch between HTML, CSS, and JS tabs
-function switchTab(tab) {
-    if (tab === 'html') {
-        document.getElementById("html-editor").parentElement.style.display = 'block';
-        document.getElementById("css-editor").parentElement.style.display = 'none';
-        document.getElementById("js-editor").parentElement.style.display = 'none';
-    } else if (tab === 'css') {
-        document.getElementById("html-editor").parentElement.style.display = 'none';
-        document.getElementById("css-editor").parentElement.style.display = 'block';
-        document.getElementById("js-editor").parentElement.style.display = 'none';
-    } else {
-        document.getElementById("html-editor").parentElement.style.display = 'none';
-        document.getElementById("css-editor").parentElement.style.display = 'none';
-        document.getElementById("js-editor").parentElement.style.display = 'block';
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-}
-
-// Function to run the code and display the result in the iframe
-function runCode() {
-    let htmlCode = htmlEditor.getValue();
-    let cssCode = "<style>" + cssEditor.getValue() + "</style>";
-    let jsCode = "<script>" + jsEditor.getValue() + "</script>";
-    let outputFrame = document.getElementById("output");
-    outputFrame.srcdoc = htmlCode + cssCode + jsCode;
+    tablinks = document.getElementsByTagName("button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
